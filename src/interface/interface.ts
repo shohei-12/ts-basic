22602325d9d0a3e56fd50f9ed9ca4fde76df41fd
shohei-12 <shoheimaeda10@gmail.com>
2021-03-09 21:48:05 +0900

    Learn inheritance of interface and type aliases

--- src/interface/interface.ts
+++ src/interface/interface.ts
@@ -29,3 +29,35 @@ const maboDon: MaboDon = {
   spicyLevel: 10,
   gram: 350,
 };
+
+// Inheritance of interface
+interface Book {
+  page: number;
+  title: string;
+}
+
+interface Magazine extends Book {
+  cycle: 'daily' | 'weekly' | 'monthly' | 'yearly';
+}
+
+const jump: Magazine = {
+  page: 300,
+  title: '週刊少年ジャンプ',
+  cycle: 'weekly',
+};
+
+// Inheritance of type aliases
+type BookType = {
+  page: number;
+  title: string;
+};
+
+interface Handbook extends BookType {
+  theme: string;
+}
+
+const cotrip: Handbook = {
+  page: 120,
+  title: 'ことりっぷ',
+  theme: '旅行',
+};
